dd059a148c6cafd51cbea22737f9aa0845b54d13
byeong gwan <gomserker@gmail.com>
2021-10-06 23:09:50 +0900

    DataStructure:LinkedList 구현하기
    개빡셈ㄷㄷ

---
+++ DataStructure/src/LinkedList/MyLinkedTest.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+public class MyLinkedTest {
+    public static void main(String[] args) {
+        
+        MyLinkedList list = new MyLinkedList();
+        
+        list.addElement("A");
+        list.addElement("B");
+        list.addElement("C");
+        list.printAll();
+        list.insertElement(3, "D");
+        list.printAll();
+        list.removeElement(1);
+        list.printAll();
+        
+        list.insertElement(0, "A-1");
+        list.printAll();
+        System.out.println(list.getSize());
+        
+        list.removeAll();
+        list.printAll();
+        list.addElement("A");
+        list.printAll();
+        System.out.println(list.getElement(0));
+        list.removeElement(0);
+    }
+    
+    
+}
